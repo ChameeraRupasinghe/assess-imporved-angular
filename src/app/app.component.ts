4567f7bd5b8c971e9f5f6d5da5b1cf5048c613e7
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2020-02-06 01:36:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assess-improved';
+  optionSetA: any = ['Pathetic', 'Bad', 'Neutral', 'Good', 'Excellent'];
+  optionSetB: any = ['Never', 'Using', 'Neutral', 'Highly-using'];
+  optionSetC: any = ['Never', 'Not-using', 'Neutral', 'Using', 'Highly-using'];
+  optionSetD: any = ['Frequent', 'Always', 'Neutral', 'Random', 'Never'];
+
+
+  constructor(public fb: FormBuilder) {
+  }
+
+  assessForm = this.fb.group({
+    equipSuitable: '',
+    skillLabour: '',
+    labourXp: '',
+    technology: '',
+    constructionMethod: '',
+    techKnowledgeStaff: '',
+    materialQuality: '',
+    opportunityChallengingTasks: '',
+    commWithLabours: '',
+    toolShortage: '',
+    materialDelay: '',
+    wages: '',
+    accommodation: '',
+    love: '',
+    qualityControl: '',
+    leadership: '',
+    breakdown: '',
+    crewSize: '',
+    law: '',
+    overtime: '',
+    bonus: '',
+    periodicMeeting: '',
+    supervisoryIncompetence: '',
+    incompleteDrawing: ''
+  });
 }
