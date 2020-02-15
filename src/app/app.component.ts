76e0af34947593c3ddb4e08d273a904598fdb19c
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2020-02-15 23:43:20 +0530

    Productivity calculation implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,53 +9,85 @@ import {FormBuilder} from '@angular/forms';
 export class AppComponent {
   title = 'assess-improved';
   optionSetA: any = [
-    {name: 'Pathetic', value: -1},
-    {name: 'Bad', value: -0.5},
-    {name: 'Neutral', value: 0},
-    {name: 'Good', value: 0.5},
-    {name: 'Excellent', value: 1}];
-  optionSetB: any = ['Never', 'Using', 'Neutral', 'Highly-using'];
-  optionSetC: any = ['Never', 'Not-using', 'Neutral', 'Using', 'Highly-using'];
-  optionSetD: any = ['Frequent', 'Always', 'Neutral', 'Random', 'Never'];
+    { name: 'Pathetic', value: -1 },
+    { name: 'Bad', value: -0.5 },
+    { name: 'Neutral', value: 0 },
+    { name: 'Good', value: 0.5 },
+    { name: 'Excellent', value: 1 }];
 
+  optionSetB: any = [
+    { name: 'Never', value: -1 },
+    { name: 'Not-using', value: -0.5 },
+    { name: 'Neutral', value: 0 },
+    { name: 'Using', value: 0.5 },
+    { name: 'Highly-using', value: 1 }];
+
+  optionSetC: any = [
+    { name: 'Never', value: -1 },
+    { name: 'Not-using', value: -0.5 },
+    { name: 'Neutral', value: 0 },
+    { name: 'Using', value: 0.5 },
+    { name: 'Highly-using', value: 1 }];
+
+  optionSetD: any = [
+    { name: 'Frequent', value: -1 },
+    { name: 'Always', value: -0.5 },
+    { name: 'Neutral', value: 0 },
+    { name: 'Random', value: 0.5 },
+    { name: 'Never', value: 1 }];
+
+  weights: number[] =
+    [7.90, 6.66, 6.24, 5.89, 5.50,
+      5.44, 5.04, 4.97, 4.77, 4.68,
+      3.33, 3.22, 2.52, 1.12, 1.07,
+      1.06, 1.05, 1.01, 0.99, 5.58,
+      5.47, 5.36, 4.34, 3.47, 3.22];
+
+  productivityString: string = "_ _%";
 
   constructor(public fb: FormBuilder) {
   }
 
   assessForm = this.fb.group({
-    equipSuitable: '',
-    skillLabour: '',
-    labourXp: '',
-    technology: '',
-    constructionMethod: '',
-    techKnowledgeStaff: '',
-    materialQuality: '',
-    opportunityChallengingTasks: '',
-    commWithLabours: '',
-    toolShortage: '',
-    materialDelay: '',
-    wages: '',
-    accommodation: '',
-    love: '',
-    qualityControl: '',
-    leadership: '',
-    breakdown: '',
-    crewSize: '',
-    law: '',
-    overtime: '',
-    bonus: '',
-    periodicMeeting: '',
-    supervisoryIncompetence: '',
-    incompleteDrawing: '',
-    rain: ''
+    equipSuitable: 0,
+    toolSatisfication: 0,
+    breakdown: 0,
+    materialQuality: 0,
+    materialAvailabilty: 0,
+    commWithLabours: 0,
+    leadership: 0,
+    crewSize: 0,
+    periodicMeeting: 0,
+    supervisoryCompetence: 0,
+    skillLabour: 0,
+    labourXp: 0,
+    overtime: 0,
+    opportunityChallengingTasks: 0,
+    wages: 0,
+    accommodation: 0,
+    love: 0,
+    law: 0,
+    bonus: 0,
+    technology: 0,
+    constructionMethod: 0,
+    techKnowledgeStaff: 0,
+    incompleteDrawing: 0,
+    qualityControl: 0,
+    rain: 0
   });
 
+
   onSubmit() {
+    let productivity = 0;
+    let sumMark = 0;
+    let i = 0;
 
-    const sum = 0;
-
-
-    alert(JSON.stringify(this.assessForm.controls['equipSuitable'].value));
+    Object.keys(this.assessForm.controls).forEach(key => {
+      sumMark = sumMark + this.assessForm.controls[key].value * this.weights[i]
+      i++;
+    });
+    productivity = 100 * (99.9 + sumMark) / 199.8;
+    this.productivityString = productivity.toFixed(2) + "%";
 
   }
 }
